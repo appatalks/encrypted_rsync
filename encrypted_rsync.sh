26691a5889d5f76d92d545bcec5fc197b323ca62
Steven <servingbaby@users.noreply.github.com>
2016-10-11 07:58:46 -0500

    Encrypted_Rsync_To_LUKS
    Hello World!

---
+++ encrypted_rsync.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#### AUTO MOUNT ENCRYPTED VOLUME ####
+echo "Sending Keyfile"
+echo "..."
+scp -q -P $PORT keyfile $IP:/home
+echo ""
+echo "Opening Encrypted Volume"
+echo "..."
+ssh -q -t -p$PORT $IP "sudo chmod 600 /home/keyfile"
+ssh -q -t -p$PORT $IP "sudo cryptsetup luksOpen /dev/xvdc encrypted --key-file /home/keyfile"
+echo ""
+echo "Removing Keyfile"
+echo "..."
+echo ""
+ssh -q -t -p$PORT $IP "sudo rm -f keyfile"
+echo "Mounting Encrypted Volume"
+echo "..."
+ssh -q -t -p$PORT $IP "sudo mount /dev/mapper/encrypted /mnt/encrypted/"
+echo ""
+echo "Running Rsync"
+echo "..."
+#### RUNNING SYNC ####
+rsync -e 'ssh -p $PORT' -aruvhq /home $IP:/mnt/encrypted/data | pv
+#### UNMOUNT AND ENCRYPT VOLUME ####
+echo ""
+echo "Rsync Complete"
+echo "..."
+echo ""
+echo "Unmounting Volume"
+echo "..."
+ssh -q -t -p$PORT $IP "sudo umount /mnt/encrypted/"
+echo ""
+echo "Encrypting Volume"
+echo "..."
+ssh -q -t -p$PORT $IP "sudo cryptsetup luksClose encrypted"
+echo ""
+echo "Completed"
